fc63fb9f93add6a06cbcfa9831995582251019c9
Johann Petzold <johann.petzold@gmail.com>
2025-05-14 13:16:05 +0200

    Initial Commit

---
+++ mlfilterimage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mlfilterimage
+//
+//  Created by Johann Petzold on 14/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mlfilterimage/mlfilterimageApp.swift
@@ -0,0 +1,17 @@
+//
+//  mlfilterimageApp.swift
+//  mlfilterimage
+//
+//  Created by Johann Petzold on 14/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct mlfilterimageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
